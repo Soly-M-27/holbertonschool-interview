eb5894de508d721481a0722185deda404bf0ebcd
Soly-M-27 <solymar227@icloud.com>
2022-08-18 21:49:24 -0400

    Forget parent

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-    if (newNode == NULL || *parent == NULL)
+    if (newNode == NULL)
         return (NULL);
 
     newNode->parent = parent;
