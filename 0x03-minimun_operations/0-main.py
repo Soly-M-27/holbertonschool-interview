ce7a4a079c8f4e1c0a66bd654120a56622df7eca
Soly-M-27 <solymar227@icloud.com>
2022-08-24 22:12:02 -0400

    Adding all on MinOperations function to return number of operations done on n numbers

---
+++ 0x03-minimun_operations/0-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+from curses.ascii import NL
+
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
---
+++ 0x03-minimun_operations/0-minoperations.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+    """
+    In a text file, there is a single character H. 
+    Your text editor can execute only two operations in this file:
+    Copy All and Paste. Given a number n, write a method that calculates
+    the fewest number of operations needed to result in exactly n H characters
+    in the file.
+    Returns: 
+        int: minimum number of operations needed
+    """
+
+
+def minOperations(n):
+    """Prototype: minOperations
+
+    Args:
+        n (int): Number of H characters to get to
+
+    Returns:
+        int: Min num of operations needed to return n number of the letter H
+    """
+    if n < 2 or type(n) is not int:
+        return 0
+    
+    a = "H"
+    x = a[0]  # the letter H
+    m = 1
+
+    paste = 0
+
+    if n % 2 == 0 and n % 3 != 0 and len(a) <= 1:
+        paste = 2
+    elif n % 2 != 0 and n % 3 == 0 and len(a) <= 1:
+        paste = 3
+    elif n % 2 == 0 and n % 3 == 0 and len(a) <= 1:
+        paste = 3
+
+    while len(a) < paste:
+        a += x
+        m += 1
+
+    rep_paste = a
+    
+    while len(a) < n:
+        if len(a) < n:
+            a += rep_paste
+            m += 1
+        if len(a) == n:
+            return m + 1
