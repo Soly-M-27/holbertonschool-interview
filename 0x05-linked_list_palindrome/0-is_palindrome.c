efcc7978e2068f1fac87ec550358fbb4578481d2
Soly-M-27 <solymar227@icloud.com>
2022-09-09 00:15:52 -0400

    Palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 if is not palindrome, 1 if it is
+ **/
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow;
+	listint_t *fast = *head;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (fast->next)
+		fast = fast->next;
+	slow = *head;
+	while (fast >= slow)
+	{
+		if (slow->n == fast->n)
+		{
+			slow = slow->next;
+			fast -= 2;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
