d887d92e5d7fda625320dae3449620b29de1fd04
Soly-M-27 <solymar227@icloud.com>
2022-12-01 16:42:49 -0400

    pycodestyle

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -8,23 +8,19 @@ def rain(walls=[]):
     zero_count = 0
     wall_extract = 0
     total_water = 0
-    
+
     if (list_len == 0):
         return 0
-     
+
     for x in walls:
         if (x > 0):
             current_wall = x
-            #print("current_wall: {:d}".format(current_wall))
             if (walls[x + 1] > 0):
                 new_wall = x + 1
-                #print("new_wall: {:d}".format(new_wall))
             if (current_wall < new_wall):
                 total_water += current_wall
             if (current_wall > new_wall):
                 total_water += new_wall
             if (total_water < 0):
                 total_water *= -1
-            #print("-------------------------")
-            #print("total_water: {:d}".format(total_water))
     return total_water
