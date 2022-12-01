e369143b4ccc0987bec0fb031cb63ddc1d836804
Soly-M-27 <solymar227@icloud.com>
2022-12-01 17:34:00 -0400

    Calculate how much rainwater was retained by the walls

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+''' Module Rain '''
+
 
 def rain(walls=[]):
-    list_len = len(walls)
-    current_wall = 0
-    new_wall = 0
+    """
+    Given a list of non-negative integers representing the heights
+    of walls with unit width 1, as if viewing the cross-section of a
+    relief map, calculate how many square units of water will be retained
+    after it rains.
+    Args:
+        walls (list): assume that all are non-negative integers
+    Returns:
+        total of water retained or 0
+    """
+    n = len(walls)
     total_water = 0
 
-    if (list_len == 0):
-        return 0
+    for i in range(1, n - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, n):
+            right = max(right, walls[j])
+        total_water = total_water + (min(left, right) - walls[i])
 
-    for x in walls:
-        if (x > 0):
-            current_wall = x
-            if (walls[x + 1] > 0):
-                new_wall = x + 1
-            if (current_wall < new_wall):
-                total_water += current_wall
-            if (current_wall > new_wall):
-                total_water += new_wall
-            if (total_water < 0):
-                total_water *= -1
     return total_water
