93d374a74ac45b691fe5bfa4cb2beb66eb547101
Soly-M-27 <solymar227@icloud.com>
2023-03-09 23:46:19 -0400

    Fixed betty documentation

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -9,12 +9,12 @@
  */
 char find_char(int col, int row)
 {
-    for (; col && row; col /= 3, row /= 3)
-    {
-        if (col % 3 == 1 && row % 3 == 1)
-            return (' ');
-    }
-    return ('#');
+	for (; col && row; col /= 3, row /= 3)
+	{
+		if (col % 3 == 1 && row % 3 == 1)
+			return (' ');
+	}
+	return ('#');
 }
 
 /**
@@ -25,16 +25,16 @@ char find_char(int col, int row)
 
 void menger(int level)
 {
-    int col, row, size;
+	int col, row, size;
 
-    if (level >= 0)
-    {
-        size = pow(3, level);
-        for (col = 0; col < size; col++)
-        {
-            for (row = 0; row < size; row++)
-                printf("%c", find_char(col, row));
-            printf("\n");
-        }
-    }
+	if (level >= 0)
+	{
+		size = pow(3, level);
+		for (col = 0; col < size; col++)
+		{
+			for (row = 0; row < size; row++)
+				printf("%c", find_char(col, row));
+			printf("\n");
+		}
+	}
 }
