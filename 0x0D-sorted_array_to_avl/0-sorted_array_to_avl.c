1b4da3907dfae1563038f7c8f3afcb250fe1a3c8
Soly-M-27 <solymar227@icloud.com>
2023-03-30 22:15:08 -0300

    Fixed betty documentation, indentation of tabs after converting indentation to tabs

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -10,18 +10,19 @@
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    avl_t *header = NULL;
-    if (array == NULL)
-    {
-        return (NULL);
-    }
+	avl_t *header = NULL;
 
-    if (nodes_binary(array, 0, (int)size - 1, &header) == NULL)
-    {
-        return (NULL);
-    }
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 
-    return (header);
+	if (nodes_binary(array, 0, (int)size - 1, &header) == NULL)
+	{
+		return (NULL);
+	}
+
+	return (header);
 }
 /**
  * nodes_binary - Creates binary tree
@@ -35,35 +36,36 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
  */
 avl_t *nodes_binary(int *array, int p1, int p2, avl_t **header)
 {
-    avl_t *new_node = NULL, *left = NULL, *right = NULL;
-    int middle;
-    if (p1 > p2)
-    {
-        return (NULL);
-    }
-    middle = (p2 + p1) / 2;
-    nodes_binary(array, p1, middle - 1, &left);
-    nodes_binary(array, middle + 1, p2, &right);
-    new_node = malloc(sizeof(avl_t));
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
+	avl_t *new_node = NULL, *left = NULL, *right = NULL;
+	int middle;
 
-    new_node->n = array[middle];
-    new_node->parent = NULL;
-    new_node->left = left;
-    new_node->right = right;
+	if (p1 > p2)
+	{
+		return (NULL);
+	}
+	middle = (p2 + p1) / 2;
+	nodes_binary(array, p1, middle - 1, &left);
+	nodes_binary(array, middle + 1, p2, &right);
+	new_node = malloc(sizeof(avl_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    if (left != NULL)
-    {
-        left->parent = new_node;
-    }
-    if (right != NULL)
-    {
-        right->parent = new_node;
-    }
+	new_node->n = array[middle];
+	new_node->parent = NULL;
+	new_node->left = left;
+	new_node->right = right;
 
-    *header = new_node;
-    return (new_node);
+	if (left != NULL)
+	{
+		left->parent = new_node;
+	}
+	if (right != NULL)
+	{
+		right->parent = new_node;
+	}
+
+	*header = new_node;
+	return (new_node);
 }
