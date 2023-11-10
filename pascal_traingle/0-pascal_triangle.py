5936f154ad6abfbf78bd5e3fd2839e9616a33ac8
Soly-M-27 <solymar227@icloud.com>
2023-11-09 20:02:52 -0400

    Adding 0-pascal_triangle.py file where function creates a list of lists with n being the limit of the Pascal Triangle to be created. Made empty list to be checked after adding a new row per iteration, until n is reached

---
+++ pascal_traingle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''' Module Pascal's Triangle '''
+
+
+def pascal_triangle(n):
+	"""
+	Function that returns a list of lists of integers representing
+	the Pascal's triangle o n.
+	Args:
+		n (int): Limit for the Pascal Triangle to be created
+	Returns:
+		Pascal's Triangle of n or an empty list if n <= 0 
+	"""
+	if n <= 0:
+		return []
+
+	triangle = []
+	for i in range(n):
+		row = [1]
+
+		if triangle:
+			prev_row = triangle[-1]
+			for j in range(len(prev_row) - 1):
+				row.append(prev_row[j] + prev_row[j + 1])
+			
+			row.append(1)
+
+		triangle.append(row)
+
+	return triangle
